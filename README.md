1b874dc013da3e075787e89d7eea60a3cd8e014e
xiesy <2847426033@qq.com>
2021-05-26 12:00:34 +0800

    修改添加备注

--- README.md
+++ README.md
@@ -1,10 +1,50 @@
-<<<<<<< HEAD
-## 执行 npm i
-## 执行 npm run all
-### jade 
-* view文件下的html是分开的
-* main文件下的html是合并的
-=======
-# mytemplate
-模板整理保存
->>>>>>> 3708c2312dc1da5f6d04335146b1af3d42832ddf
+ 
+# gulp pug stylus
+
+#### 安装
+
+```
+npm i
+
+```
+
+#### 启动
+
+```
+npm run all
+```
+
+
+## jade 
+* src/view文件下的html是分开的
+* src/main文件下的html是合并的
+
+
+
+## 遇到的问题
+
+### 关于“primordials is not defined”报错的解决办法
+   
+也许是因为你的gulp版本v3，node版本v12。
+
+* 解决方法：
+  - 将gulp版本升级到v4。
+  - 将node版本降级到v11。
+  - 将graceful-fs升级到在node v12+下工作的版本4.2.2(推荐)。
+
+* 推荐解决办法：
+
+  1. 在package.json同级目录下新建文件npm-shrinkwrap.json，输入以下内容：
+
+    ```
+    {
+    "dependencies": {
+    "graceful-fs": {
+    "version": "4.2.2"
+    }
+    }
+    }
+    ```
+
+  2. npm install
+
--- config/index.js
+++ config/index.js
@@ -1,3 +1,8 @@
+/*
+ * @Description:
+ * @Version: 1.0
+ * @LastEditTime: 2021-05-26 11:39:23
+ */
 /**
  * 网站配置信息
  */
@@ -11,7 +16,7 @@ module.exports = {
         // jade -> html 需要监听转换的jade文件路径
         jadeTohtmlView: './src/view/view/*.pug',
         // html 输出的路径
-        jadeTodistView: './dist'
+        jadeTodistView: './dist',
     },
     styl: {
         stylTocssHtml: './src/assets/css/html/*.styl',
@@ -23,21 +28,21 @@ module.exports = {
         // styl 转换完成后，需要将多个文件合并成一个的文件名
         stylToFileName: 'main.css',
         // css 输出的路径
-        stylTodist: './dist/css/'
+        stylTodist: './dist/css/',
     },
     watch: {
         // gulp 需要监听的文件路径，当我们在src修改源码时，gulp会动态编译输出到dist
-        watchFile: 'src/**/*'
+        watchFile: 'src/**/*',
     },
     nodeService: {
         // 访问ip
         // host: "http://127.0.0.1:",
-        host: "http://localhost:",
+        host: 'http://localhost:',
         // 访问端口
         port: 1314,
         // 提示信息
         log: '项目运行在:',
         // 是否开始自动打开浏览器功能，取值：true，false
-        isOpenBrowser: true
-    }
-};
+        isOpenBrowser: true,
+    },
+}
